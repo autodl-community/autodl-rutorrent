3aacfe3ba44ff74932c1832ce28cf66c0e6ce393
thebigmunch <munchicus@gmail.com>
2014-04-06 13:15:04 -0400

    Remove reload button [#7]

--- AutodlIrssiTab.js
+++ AutodlIrssiTab.js
@@ -87,7 +87,6 @@ function AutodlIrssiTab(dialogManager, plugin)
 				'<input type="button" id="autodl-log-update-button" class="Button" value="' + theUILang.autodlUpdate1 + '" title="' + theUILang.autodlUpdate2  + '" />' +
 				'<input type="button" id="autodl-log-whatsnew-button" class="Button" value="' + theUILang.autodlWhatsNew1 + '" title="' + theUILang.autodlWhatsNew2  + '" />' +
 				'<input type="button" id="autodl-log-version-button" class="Button" value="' + theUILang.autodlVersion1 + '" title="' + theUILang.autodlVersion2  + '" />' +
-				'<input type="button" id="autodl-log-reload-button" class="Button" value="' + theUILang.autodlReload1 + '" title="' + theUILang.autodlReload2  + '" />' +
 				'<input type="button" id="autodl-log-reload-trackers-button" class="Button" value="' + theUILang.autodlReloadTrackers1 + '" title="' + theUILang.autodlReloadTrackers2  + '" />' +
 				'<input type="button" id="autodl-log-backup-button" class="Button" value="' + theUILang.autodlBackup1 + '" title="' + theUILang.autodlBackup2  + '" />' +
 				'<form target="autodl-restore-iframe" id="autodl-restore" method="POST" action="plugins/autodl-irssi/writeconfig.php" enctype="multipart/form-data">' +
@@ -129,10 +128,6 @@ function AutodlIrssiTab(dialogManager, plugin)
 	$("#autodl-log-version-button").click(function()
 	{
 		this_._sendAutodlCommand("version");
-	});
-	$("#autodl-log-reload-button").click(function()
-	{
-		this_._sendAutodlCommand("reload");
 	});
 	$("#autodl-log-reload-trackers-button").click(function()
 	{

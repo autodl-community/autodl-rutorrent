84ff8d4f989e361287ef417e03232e42cb9eff5e
thebigmunch <mail@thebigmunch.me>
2016-02-13 15:24:41 -0500

    Get styl files automatically for compiling rather than hardcoding

--- buildcss.sh
+++ buildcss.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
+shopt -s extglob
+
 root_dir=$(git rev-parse --show-toplevel) || eval 'echo "This script must be run from within the autodl-rutorrent repo directory." 1>&2; exit 1'
 styles_dir="$root_dir/styles"
 css_dir="$root_dir/css"
+style_files="$styles_dir/!(base).styl"
 
-stylus $styles_dir/acid.styl -o $css_dir/acid.css
-stylus $styles_dir/acid.styl -o $css_dir/acid-min.css -c
+for style in $style_files
+do
+	name=$(basename --suffix=.styl $style)
 
-stylus $styles_dir/blue.styl -o $css_dir/blue.css
-stylus $styles_dir/blue.styl -o $css_dir/blue-min.css -c
-
-stylus $styles_dir/dark.styl -o $css_dir/dark.css
-stylus $styles_dir/dark.styl -o $css_dir/dark-min.css -c
-
-stylus $styles_dir/excel.styl -o $css_dir/excel.css
-stylus $styles_dir/excel.styl -o $css_dir/excel-min.css -c
-
-stylus $styles_dir/oblivion.styl -o $css_dir/oblivion.css
-stylus $styles_dir/oblivion.styl -o $css_dir/oblivion-min.css -c
-
-stylus $styles_dir/standard.styl -o $css_dir/standard.css
-stylus $styles_dir/standard.styl -o $css_dir/standard-min.css -c
+	stylus $style -o $css_dir/$name.css
+	stylus $style -o $css_dir/$name-min.css -c
+done
